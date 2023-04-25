fb7dc38150a19a5fb333da2de1e954aa2df9a885
GritLs <1915619724@qq.com>
2023-04-25 16:12:06 +0800

    将模型从text-davinci-003改为gpt3.5-turbo

---
+++ gpt_module/api_keys.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+class ApiKeys:
+    def __init__(self, file_path='D:/vscode_prj/GPTSpeaker/gpt_module/keys.json'):
+        self.file_path = file_path
+        self.keys = self.load_keys()
+
+    def load_keys(self):
+        with open(self.file_path, 'r') as f:
+            keys = json.load(f)
+        return keys
+
+    def set_key_as_env_var(self, key):
+        if key in self.keys:
+            os.environ[key] = self.keys[key]
+        else:
+            raise KeyError(f"Key '{key}' not found in {self.file_path}")
+
+    def get_key(self, key):
+        if key in self.keys:
+            return self.keys[key]
+        else:
+            raise KeyError(f"Key '{key}' not found in {self.file_path}")
+
+    def set_all_keys_as_env_vars(self):
+        for key in self.keys:
+            os.environ[key] = self.keys[key]
+
--- gpt_module/chat.py
+++ gpt_module/chat.py
@@ -1,81 +1,66 @@
 import os
-import json
-from custom_tools import IndoorTemperatureHumidity,CheckRaining
-from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+from langchain.agents import Tool
 from langchain.memory import ConversationBufferMemory
-from langchain import OpenAI, LLMChain
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from api_keys import ApiKeys
+from langchain.chat_models import PromptLayerChatOpenAI
 from langchain.utilities import (
-    GoogleSearchAPIWrapper,
     OpenWeatherMapAPIWrapper,
     WolframAlphaAPIWrapper,
-    PythonREPL
+    PythonREPL,
+    WikipediaAPIWrapper
 )
 
-class Assistant:
-    def __init__(self,keys_path):
-        self.memory = ConversationBufferMemory(memory_key="chat_history")
-        #导入API_KEY
-        self.setup_environment_variables(keys_path)
-        #设置工具集
-        self.search = GoogleSearchAPIWrapper()
-        self.weather = OpenWeatherMapAPIWrapper()
-        self.wolframalpha = WolframAlphaAPIWrapper()
-        self.pythonREPL = PythonREPL()
-        self.tools = [
-            Tool(
-                name="Search",
-                func=self.search.run,
-                description="useful for when you need to answer questions about current events or the current state of the world.",
-            ),
-            Tool(
-                name="Weather",
-                func=self.weather.run,
-                description='''useful for when you need to answer the question about current weather. If human didn't specify a location, you'll need to pass in "Nanjing,CN" as the default argument''',
-            ),
-            Tool(
-                name="Wolfram",
-                func=self.wolframalpha.run,
-                description="Use it when you need to solve some mathematical problems and you can use natural language (msut be English) input. And you need to convert its output into a format that can be read using speech synthesis software.",
-            ),
-            Tool(
-                name="PythonREPL",
-                func=self.pythonREPL.run,
-                description="You can write python code and execute it, for example if you want to know the system time you can pass python code to this to get the current system time."
-            ),
-            IndoorTemperatureHumidity(),
-            CheckRaining()
-        ]
+api = ApiKeys()
+api.set_all_keys_as_env_vars()
 
-        prefix = '''Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:'''
-        suffix = """Remember you are an artificial intelligence assistant, your Chinese name is 小爱, you are friendly to humans and are a good helper to them, you have a very lively personality. Remember you are also a chatterbox (really important!). Your owner is Chinese and does not understand English, so your can think in English, but your final answer must be in Chinese.Begin!" \n {chat_history}\n Question: {input}\n{agent_scratchpad}"""
-
-        self.prompt = ZeroShotAgent.create_prompt(
-            self.tools,
-            prefix=prefix,
-            suffix=suffix,
-            input_variables=["input", "chat_history", "agent_scratchpad"],
-        )
-
-        self.llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=self.prompt, verbose=True)
-        self.agent = ZeroShotAgent(llm_chain=self.llm_chain, tools=self.tools,verbose = True)
-        self.agent_chain = AgentExecutor.from_agent_and_tools(
-            agent=self.agent, tools=self.tools, memory=self.memory,verbose=True
-        )
-    @staticmethod
-    def setup_environment_variables(keys_path):
-        with open(keys_path) as f:
-            keys = json.load(f)
-        os.environ["GOOGLE_CSE_ID"] = keys["GOOGLE_CSE_ID"]
-        os.environ["GOOGLE_API_KEY"] = keys["GOOGLE_API_KEY"]
-        os.environ["OPENWEATHERMAP_API_KEY"] = keys["OPENWEATHERMAP_API_KEY"]
-        os.environ["OPENAI_API_KEY"] = keys["OPENAI_API_KEY"]
-        os.environ["WOLFRAM_ALPHA_APPID"] = keys["WOLFRAM_ALPHA_APPID"]
+search = SerpAPIWrapper()
+weather = OpenWeatherMapAPIWrapper()
+wolframalpha = WolframAlphaAPIWrapper()
+pythonREPL = PythonREPL()
+wikipedia = WikipediaAPIWrapper()
+tools = [
+    Tool(
+        name = "Search",
+        func=search.run,
+        description="当你需要回答有关当前事件或世界现状的问题时请使用该工具"
+    ),
+    Tool(
+        name="Weather",
+        func=weather.run,
+        description='''在你需要回答关于当前天气的问题时使用该工具。如果主人没有指定一个地点，你需要传入 "Nanjing,CN" 作为action_input''',
+    ),
+    Tool(
+        name="Wolfram",
+        func=wolframalpha.run,
+        description="当你需要解决一些数学问题时请使用该工具，同时你必须将问题转化为数学语言。",
+    ),
+    Tool(
+        name="PythonREPL",
+        func=pythonREPL.run,
+        description="你可以编写Python代码并执行它。例如，如果你想知道系统时间，你可以将相关Python代码传递给它，以获得当前的系统时间。"
+    ),
+    Tool(
+        name="Wikipedia",
+        func=wikipedia.run,
+        description="这是维基百科的api，如果你觉得需要使用维基百科请使用这个工具。"
+    )
+]
 
 
-    def run(self, input_str):
-        return self.agent_chain.run(input=input_str)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 
-# keys_path = ''
-# Assistant = Assistant(keys_path)
-# xiao_ai.run("What is the current indoor temperature and humidity?")
+llm = PromptLayerChatOpenAI(pl_tags=["langchain"],callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),verbose=True,streaming=True,temperature=0.9)
+
+agent_chain = initialize_agent(tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,  memory=memory)
+
+agent_chain.run(input="你好")
+agent_chain.run(input="现在的美国总统是谁？")
+agent_chain.run(input="sqrt(12)=?")
+agent_chain.run(input="韦达定理是什么？")
+agent_chain.run(input="现在天气如何？")
